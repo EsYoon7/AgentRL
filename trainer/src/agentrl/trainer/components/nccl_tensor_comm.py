bdcbda755aec79db9756e061689e1910a20dfd48
Yanyu Ren <yanyu.ren@aminer.cn>
2025-11-10 05:32:05 +0000

    fix: designate a device when multiple in one node

--- trainer/src/agentrl/trainer/components/nccl_tensor_comm.py
+++ trainer/src/agentrl/trainer/components/nccl_tensor_comm.py
@@ -18,13 +18,13 @@ def send_buffer(buffer: TensorBuffer, pg):
         dist.broadcast(v, group_src=0, group=pg)
 
 
-def receive_buffer(pg) -> TensorBuffer:
+def receive_buffer(pg, device=None) -> TensorBuffer:
     buffer = []
     lst: list[None | dict] = [None]
-    broadcast_object_list(lst, group_src=0, group=pg)
+    broadcast_object_list(lst, group_src=0, group=pg, device=device)
     descriptions: dict = lst[0]
     for k, (shape, dtype) in descriptions.items():
-        v = torch.empty(shape, dtype=dtype, device='cuda')
+        v = torch.empty(shape, dtype=dtype, device=device)
         dist.broadcast(v, group_src=0, group=pg)
         buffer.append((k, v))
     return buffer
@@ -42,6 +42,7 @@ class NCCLTensorSender:
                 world_size=world_size,
                 rank=0,
                 group_name=f"nccl_comm_{addr}_{port}",
+                device_id=torch.device("cuda:0")
             )
 
     def send(self, bucket_size):
@@ -78,12 +79,14 @@ class NCCLTensorSender:
 class NCCLTensorReceiver:
     def __init__(self, worker: AbstractAsyncRolloutWorker, addr, port, world_size, offset):
         self.worker = worker
+        self.device = torch.device(f"cuda:{(offset + worker.rank) % torch.cuda.device_count()}") # TODO: this is a temperory hack for multiple devices in one process. 
         self.pg = init_custom_process_group(
             backend="nccl",
             init_method=f"tcp://{addr}:{port}",
             world_size=world_size,
             rank=offset + worker.rank,
             group_name=f"nccl_comm_{addr}_{port}",
+            device_id=self.device
         )
 
     async def async_receive(self):
@@ -92,12 +95,12 @@ class NCCLTensorReceiver:
         await self.worker.async_acquire_writer_lock()
         task = asyncio.sleep(0)
         while not done:
-            buffer = await asyncio.to_thread(receive_buffer, self.pg)
+            buffer = await asyncio.to_thread(receive_buffer, self.pg, self.device)
             await task
             task = asyncio.create_task(self.worker.update_params(buffer))
 
             lst: list[bool | None] = [None]
-            await asyncio.to_thread(broadcast_object_list, lst, group_src=0, group=self.pg)
+            await asyncio.to_thread(broadcast_object_list, lst, group_src=0, group=self.pg, device=self.device)
             done: bool = lst[0]
         await task
         await self.worker.async_release_writer_lock()
